431d30fac9e44bd6fe01b7ad2d08631cb501a16c
filippovpik@outlook.com <filippovpik@outlook.com>
2025-08-24 14:51:37 +0300

    Добавьте файлы проекта.

---
+++ Task10_1/Models/AuthModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10_1.Models
+{
+    internal static class AuthModel
+    {
+        const string ValidUsername = "admin";
+        const string ValidPassword = "12345";   
+    
+    public static bool Authenticate(string username, string password)
+        {
+            if (username == ValidUsername && password == ValidPassword)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+
+
+    
